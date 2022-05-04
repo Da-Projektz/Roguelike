36c1d075279002e829947f09b52249bba8d332be
Dru <awalsh0829@gmail.com>
2022-05-03 21:09:49 -0600

    Feature add pygame, remove tcod Fixes #18

--- settings.py
+++
@@ -1,7 +0,0 @@
-class Settings:
-    """general game settings"""
-
-    def __init__(self):
-        self.screen_width = 100
-        self.screen_height = 100
-        self.game_name = "Falling for Eternity"
