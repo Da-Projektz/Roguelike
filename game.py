63a7711553ee08ea23ea9d3c03d6aaa93efbe9b8
Bryce Carson <bcars268@mtroyal.ca>
2022-05-10 15:21:30 -0600

    Fix cursor surface alpha, remove `exit(0)`

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame as pg
 
 dwarven_gauntlet = pg.image.load("dwarven_gauntlet.png")
-dwarven_gauntlet_surface = pg.surface.Surface((28, 32))
+dwarven_gauntlet_surface = pg.surface.Surface((28, 32), pg.SRCALPHA)
 dwarven_gauntlet_cursor = pg.Cursor((2, 0), dwarven_gauntlet_surface) # Tip of the forefinger is the hotspot.
 
 
@@ -13,9 +13,8 @@ def main():
     pg.init()
     pg.display.set_mode((800, 600))
 
+    dwarven_gauntlet.convert_alpha()
     dwarven_gauntlet_surface.convert_alpha()
-    # FIXME: and TODO: how does alpha work?
-    # dwarven_gauntlet_surface.fill(color=pg.Color(0, 0, 0, 255)) # Make the surface transparent, except where we actually draw
     pg.Surface.blit(dwarven_gauntlet_surface, dwarven_gauntlet, (0, 0))
     pg.mouse.set_cursor(dwarven_gauntlet_cursor)
 
@@ -25,4 +24,3 @@ def main():
             if event.type == pg.QUIT:
                 to_quit = True
                 pg.quit()
-                exit(0) # Explicitly return zero when the game was closed cleanly.
