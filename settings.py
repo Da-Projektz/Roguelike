89a525305fd5d0e9547cef9d783336124ef70493
thescientist <the-scientist@rootstorm.com>
2022-05-27 21:18:21 -0600

    Add bullets allowed 10

--- settings.py
+++ settings.py
@@ -15,4 +15,4 @@ class Settings:
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (0xff, 0, 0)
-        self.bullets_allowed = 3
+        self.bullets_allowed = 10
